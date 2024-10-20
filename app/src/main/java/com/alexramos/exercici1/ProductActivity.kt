8e035657caae6aae790692b213c5365e40f0e981
alexramosesp <alex.ramos@institutvidreres.cat>
2024-10-20 20:38:41 +0200

    Exercise Complete

--- app/src/main/java/com/alexramos/exercici1/ProductActivity.kt
+++ app/src/main/java/com/alexramos/exercici1/ProductActivity.kt
@@ -12,25 +12,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class ProductActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_product)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val fullName: EditText = findViewById(R.id.fullName)
-        val signUpButton: Button = findViewById(R.id.signUpButton)
-
-        signUpButton.setOnClickListener {
-            Toast.makeText(this, "Cuenta creada para ${fullName.text}", Toast.LENGTH_SHORT).show()
-        }
-
-
     }
 }
